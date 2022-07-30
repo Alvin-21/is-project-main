519a93714986f6091eee9d74926a79f00cb6bd38
Alvin <alvinawuor8@gmail.com>
2022-07-30 04:35:44 +0300

    Fix bugs for case upload.

--- case-file-backend.php
+++ case-file-backend.php
@@ -1,6 +1,11 @@
 <?php
 
-    include("process.php");
+    session_start();
+
+    // initializing variables
+    $errors = array();
+
+    include("connection.php");
 
     if (isset($_POST['upload_file'])) {
         // receive all input values from the form
@@ -9,42 +14,31 @@
         $description = mysqli_real_escape_string($db,$_POST['description']);
         $case_file = mysqli_real_escape_string($db,$_POST['case_file']);
 
-        // echo "<pre>"; print_r($_SESSION['user_details']); die;
+        $user_id = $_SESSION['user_details']['user_id'];
+        $file_check_query = "SELECT * FROM `case_file`";
+        $result = mysqli_query($db, $file_check_query);
 
-        // form validation: ensure that the form is correctly filled ...
-        // by adding (array_push()) corresponding error unto $errors array
-        if (empty($file_name)) { array_push($errors, "File name is required"); }
-        if (empty($case_number)) { array_push($errors, "Case number is required"); }
-        if (empty($description)) { array_push($errors, "Description is required"); }
-        if (empty($case_file)) { array_push($errors, "Case file is required"); }
-
-        $user_id1 = $_SESSION['user_details']['user_id'];
-        $file_check_query = "SELECT * FROM case_file WHERE user_id='$user_id1' LIMIT 1";
-        $result1 = mysqli_query($db, $file_check_query);
-        $case = mysqli_fetch_assoc($result1);
-        // echo "<pre>"; print_r($case['case_number']); die;
-
-    
-        if ($case) { // if user exists
+        while ($case= mysqli_fetch_assoc($result)) {
             if ($case['case_number'] === $case_number) {
-            array_push($errors, "Case number already exists");
+                array_push($errors, "Case number already exists");
+                $_SESSION['err']  = "Case number already exists";
+                header('Location: case-file.php');
             }
-            echo "Case number already exists";
         }
 
         // Finally, register user if there are no errors in the form
         if (count($errors) == 0) {
 
-            $query1 = "INSERT INTO case_file (file_id, user_id, case_number, file_name, file, description) 
-                    VALUES(null, '$user_id1', '$case_number', '$file_name', '$case_file', '$description')";
+            $query = "INSERT INTO case_file (file_id, user_id, case_number, file_name, file, description) 
+                    VALUES(null, '$user_id', '$case_number', '$file_name', '$case_file', '$description')";
 
-            $is_inserted1 = mysqli_query($db, $query1);
+            $is_inserted = mysqli_query($db, $query);
 
             // Redirects user to the appropriate page after registration
-            if ($is_inserted1) {
-            header('Location: admin-page.php');
+            if ($is_inserted) {
+                header('Location: admin-page.php');
             } else {
-                array_push($errors, "Wrong username/password");
+                header('Location: case-file.php');
             }
         }
     }
--- case-file.php
+++ case-file.php
@@ -1,67 +1,73 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['is_logged_in'])) {
-    header('location: login.php');
-}
-?>
-
 <?php include('process.php') ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-		<style>
-			<?php include "css/style.css" ?>
-		</style>
-        <title>CMS | Case Upload</title>
-    </head>
-    <body>
-        <section class="ftco-section">
-			<div class="container">
-				<div class="row justify-content-center">
-					<div class="col-md-6 col-lg-4">
-						<div class="my-3">
-							<h2 class="heading-section">Case Upload Form</h2>
-						</div>
-						<div class="login-wrap py-0">
-							<form method="post" action="case-file-backend.php">
-								<?php include('errors.php'); ?>
-								<div class="form-group mb-3">
-									<label class="form-label">File Name</label>
-									<input type="text" name="file_name" class="form-control" required>
-								</div>
-								<div class="form-group mb-3">
-									<label class="form-label">Case Number</label>
-									<input type="text" name="case_number" class="form-control" required>
-								</div>
-								<div class="form-group mb-3">
-									<label class="form-label">Case Description</label>
-									<input type="text" name="description" class="form-control" required>
-								</div>
-								<div class="form-group mb-3">
-									<label class="form-label">Case File</label>
-									<input type="file" name="case_file" class="form-control" required>
-								</div>
-								<div class="form-group mb-3">
-									<button type="submit" name="upload_file" class="btn form-control btn-primary rounded submit px-3 btn-custom">Upload</button>
-								</div>
-							</form>
-						</div>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<style>
+		<?php include "css/style.css" ?>
+	</style>
+	<title>CMS | Case Upload</title>
+</head>
+
+<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 col-lg-4">
+					<div class="my-3">
+						<h2 class="heading-section">Case Upload Form</h2>
+					</div>
+					<div class="login-wrap py-0">
+						<form method="post" action="case-file-backend.php">
+							<div>
+								<?php if (isset($_SESSION['err'])) : ?>
+									<div class="error">
+										<p>
+											<?php
+											echo $_SESSION['err'];
+											unset($_SESSION['err']);
+											?>
+										</p>
+									</div>
+								<?php endif ?>
+							</div>
+							<div class="form-group mb-3">
+								<label class="form-label">File Name</label>
+								<input type="text" name="file_name" class="form-control" required>
+							</div>
+							<div class="form-group mb-3">
+								<label class="form-label">Case Number</label>
+								<input type="text" name="case_number" class="form-control" required>
+							</div>
+							<div class="form-group mb-3">
+								<label class="form-label">Case Description</label>
+								<input type="text" name="description" class="form-control" required>
+							</div>
+							<div class="form-group mb-3">
+								<label class="form-label">Case File</label>
+								<input type="file" name="case_file" class="form-control" required>
+							</div>
+							<div class="form-group mb-3">
+								<button type="submit" name="upload_file" class="btn form-control btn-primary rounded submit px-3 btn-custom">Upload</button>
+							</div>
+						</form>
 					</div>
 				</div>
 			</div>
-		</section>
+		</div>
+	</section>
 
-		<!-- Jquery JS -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<!-- Jquery JS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
-		<!-- Bootstrap JS -->
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	<!-- Bootstrap JS -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-		<!-- Script JS -->
-		<script src="js/script.js"></script>
-    </body>
+	<!-- Script JS -->
+	<script src="js/script.js"></script>
+</body>
+
 </html>
